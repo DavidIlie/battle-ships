fdce54956d67c6baa8f3d98086288b7c105eb239
David Ilie <david@davidilie.com>
2022-11-15 09:57:11 +0000

    initial commit

---
+++ src/pages/index.tsx
@@ -0,0 +1,11 @@
+import type { NextPage } from "next";
+
+const Home: NextPage = () => {
+   return (
+      <div className="container max-w-4xl py-12 mx-auto">
+         <h1 className="text-xl font-medium">Battle Ships Game</h1>
+      </div>
+   );
+};
+
+export default Home;
