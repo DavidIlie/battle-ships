668d86d0e0184c32814a4ee11398df062ba16a52
David Ilie <david@davidilie.com>
2022-12-06 10:43:04 +0000

    fix error

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -24,6 +24,7 @@ const Game: NextPage = () => {
                            : "available"
                      }
                      changeState={setSelectedPlaceCarrier}
+                     key={index}
                   />
                ))}
             </div>
